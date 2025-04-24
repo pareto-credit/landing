3868444cada5f78bb2cce9dda70cc8ef627b3f59
Samster91 <samster91>
2025-04-24 15:09:31 +0200

    Updates

--- js/index-ms5E3LqI.js
+++ js/index-ms5E3LqI.js
@@ -1,7 +1,7 @@
 import { d as createAstro, c as createComponent, r as renderTemplate, a as renderComponent } from './astro/server-CgvuF3dj.js';
 import 'kleur/colors';
-import { c as createStaticPathGetter } from './section-CJzLl3Tv.js';
-import $$Index$1 from './index-Bp6D2S5U.js';
+import { c as createStaticPathGetter } from './section-CEJarSuz.js';
+import $$Index$1 from './index-BWNRQf6n.js';
 
 const $$Astro = createAstro("https://real-not-now.web.app/");
 const getStaticPaths = createStaticPathGetter();
