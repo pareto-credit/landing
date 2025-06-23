9b373162120b77941db4d73a5a5085c65da51ae0
Nicolò <nicandman@gmail.com>
2025-06-23 18:17:23 +0200

    Updates

--- js/404-CKR5EIK9.js
+++ js/404-CKR5EIK9.js
@@ -1,6 +1,6 @@
 import { c as createComponent, r as renderTemplate, a as renderComponent, m as maybeRenderHead, b as addAttribute, d as createAstro } from './astro/server-CgvuF3dj.js';
 import 'kleur/colors';
-import { $ as $$Section, P as Page404, a as $$Page } from './section-BcVrvv3M.js';
+import { $ as $$Section, P as Page404, a as $$Page } from './section-D8ZIy16k.js';
 /* empty css                      */
 
 const visual = "/assets/img/404-bg-Ca5pfBEg.png";
@@ -9,7 +9,7 @@ const visualTablet = "/assets/img/404-bg-tablet-CZ0FAmCf.png";
 
 const $$View = createComponent(($$result, $$props, $$slots) => {
   return renderTemplate`${renderComponent($$result, "Section", $$Section, { "className": "page-404__section", "id": "page-404" }, { "default": ($$result2) => renderTemplate` ${maybeRenderHead()}<div class="container"> <div class="left-wrap"> <div class="title-wrap"> <h2 class="title-h2 emerald">404</h2> <p class="title-h3 emerald">Something went wrong</p> </div> <ul class="menu-list"> <li class="menu-list__item"> <a href="/" class="nav up-text emerald">Home</a> </li> <li class="menu-list__item"> <a href="/" class="nav up-text emerald">Product</a> </li> <li class="menu-list__item"> <a href="/" class="nav up-text emerald">Features</a> </li> <li class="menu-list__item"> <a href="/" class="nav up-text emerald">Partners</a> </li> </ul> </div> <div class="visual"> <img${addAttribute(visual, "src")} alt="404-visual" class="desktop"> <img${addAttribute(visualTablet, "src")} alt="404-visual-tablet" class="tablet"> </div> </div> ` })}`;
-}, "/Users/samuelecester/Desktop/git/pareto-landing/src/components/sections/page404/main/view.astro", void 0);
+}, "/Users/nikiman/Projects/pareto-landing/src/components/sections/page404/main/view.astro", void 0);
 
 const $$Astro = createAstro("https://pareto.credit/");
 const $$404 = createComponent(($$result, $$props, $$slots) => {
@@ -17,9 +17,9 @@ const $$404 = createComponent(($$result, $$props, $$slots) => {
   Astro2.self = $$404;
   const { locale = "en" } = Astro2.props;
   return renderTemplate`${renderComponent($$result, "PageLayot", $$Page, { ...Page404, "locale": locale, "isActiveFooter": false, "isActiveMobileMenu": false, "isActivePreloader": false }, { "default": ($$result2) => renderTemplate` ${renderComponent($$result2, "MainSection", $$View, {})} ` })}`;
-}, "/Users/samuelecester/Desktop/git/pareto-landing/src/pages/404.astro", void 0);
+}, "/Users/nikiman/Projects/pareto-landing/src/pages/404.astro", void 0);
 
-const $$file = "/Users/samuelecester/Desktop/git/pareto-landing/src/pages/404.astro";
+const $$file = "/Users/nikiman/Projects/pareto-landing/src/pages/404.astro";
 const $$url = "/404";
 
 export { $$404 as default, $$file as file, $$url as url };
